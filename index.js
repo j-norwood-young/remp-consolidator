0f3fb955a558eb1e954d0c8ceb39244175461742
Jason Norwood-Young <jason@10layer.com>
2019-06-03 12:06:32 +0200

    Start with page views and events

--- index.js
+++ index.js
@@ -1,33 +1,94 @@
 require("dotenv").config();
 const http = require("http");
 const kafka = require('kafka-node');
+const elasticsearch = require("elasticsearch");
 
 const port = process.env.PORT || 3100;
 
-const requestHandler = (request, response) => {
-    console.log(request.url)
-    response.end('Hello Node.js Server!')
+const requestHandler = (req, res) => {
+    if(req.method == 'POST') {
+        let jsonString = '';
+        req.on('data', function (data) {
+            jsonString += data;
+        });
+        req.on('end', function () {
+            try {
+                let data = JSON.parse(jsonString);
+                queueData(data);
+                // console.log(data);
+            } catch(err) {
+                
+            }
+        });
+    } else {
+        res.end('This is the remp-consolidator')    
+    }
+    // console.log(request.url)
 }
 
 const server = http.createServer(requestHandler)
 
 server.listen(port, (err) => {
     if (err) {
-        return console.log('something bad happened', err)
+        return console.error('something bad happened', err)
     }
 
     console.log(`server is listening on ${port}`)
 })
 
-const kafkaClient = new kafka.KafkaClient({ kafkaHost: process.env.KAFKA_BROKER });
-const kafkaConsumer = kafka.Consumer;
+const esclient = new elasticsearch.Client({
+    host: 'localhost:9200',
+    // log: 'trace'
+});
 
-const consumer = new kafkaConsumer(kafkaClient, [
-    { topic: process.env.KAFKA_TOPIC }
-])
+const kafkaOptions = {
+	kafkaHost: '127.0.0.1:9092',
+	groupId: process.env.KAFKA_GROUP,
+	autoCommit: true,
+	autoCommitIntervalMs: 5000,
+	sessionTimeout: 15000,
+ 	fetchMaxBytes: 10 * 1024 * 1024, // 10 MB
+	protocol: ['roundrobin'],
+	fromOffset: 'earliest',
+	outOfRangeOffset: 'earliest'
+}
 
-consumer.on('message', function (message) {
-    console.log(message);
+const kafkaConsumerGroup = kafka.ConsumerGroup;
+const consumer = new kafkaConsumerGroup(kafkaOptions, process.env.KAFKA_TOPIC)
+
+var cache = [];
+
+const checkCache = async () => {
+    try {
+        if (cache.length >= 100) {
+            await esclient.bulk({ maxRetries: 5, body: cache });
+            cache = [];
+            console.log("Flushed cache");
+        }
+    } catch(err) {
+        console.error(err);
+    }
+}
+
+consumer.on('message', async (message) => {
+    try {
+        let [ d0, index, d1, json, d2, timestamp ] = message.value.split(/(^\S*)(\s_json=")(.*\})(\"\s)(\d.*$)/);
+        json = JSON.parse(json.replace(/\\/g,""));
+        if (index === "events_v2") index = "events";
+        // console.log({index, json, timestamp});
+        if (index === "pageviews" || index === "events") {
+            cache.push({
+                index: {
+                    _index: index,
+                    _type: "_doc",
+                }
+            }, json);
+        }
+        await checkCache();
+        // esclient.bulk()
+    } catch(err) {
+        console.error(err);
+    }
 });
 
 consumer.on("error", err => {
